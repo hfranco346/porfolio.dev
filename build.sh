9a12ee40ce6e1e6c540dd54b98368db39e948ff7
Henderson Franco <hfranco346@gmail.com>
2025-04-18 09:47:14 -0600

    Añadir permisos de ejecución a build.sh para despliegue en Render

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-npm install
-npm run build
+
+# Instalar paquetes sin frozen-lockfile
+echo "Instalando dependencias con pnpm sin usar frozen-lockfile..."
+pnpm install --no-frozen-lockfile
+
+# Construir el proyecto
+echo "Construyendo el proyecto..."
+pnpm run build
+
+# Mensaje de finalización
+echo "¡Compilación completada con éxito!"
